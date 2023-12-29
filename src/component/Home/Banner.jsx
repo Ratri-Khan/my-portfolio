2a8f94bf10ac496fa21c22fb1c84adbd02122c1c
ratrikhanam000 <ratrikhanam000@gmail.com>
2023-12-29 19:38:06 +0600

    banner,navbar full complete

--- src/component/Home/Banner.jsx
+++ src/component/Home/Banner.jsx
@@ -1,17 +1,74 @@
-import img from "../../assets/ratri.jpg";
+import img from "../../assets/bg3.jpg";
+import "./banner.css";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
 
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
+
 const Banner = () => {
-  return (   
-      <div className="md:flex lg-flex justify-between items-center w-10/12 m-auto">
-        <div className="md:w-3/5 lg:w-3/5 ">
-          <h2 className="text-white text-4xl font-bold font-sans">
-            Hi, I am Ratri Khanom
-          </h2>
+  return (
+    // <div className="md:flex lg-flex justify-between items-center  m-auto banner ">
+
+    //     {/* <img src={img} alt="" className="h-[520px] rounded mt-20"/> */}
+    //     <div className="md:w-3/5 lg:w-3/5 ">
+    //     <h2 className="text-white text-4xl font-bold font-sans  mt-20">
+    //       Hi, I am Ratri Khanom
+    //     </h2>
+    // <motion.div
+    //   className="mb-6 text-[30px] lg:text-[45px] font-secondary font-bold uppercase leading-[1]mx-auto lg:mx-0"
+    //   variants={fadeIn("up", 0.3)}
+    //   initial="hidden"
+    //   whileInView={"show"}
+    //   viewport={{ once: "false", amount: 0.7 }}
+    // >
+    //   {/* <span className="me-3 text-white">I am a</span> */}
+    //   <TypeAnimation
+    //     sequence={[
+    //       " MERN STACK DEVELOPER",
+    //       2000,
+    //       "FRONT-END-DEVELOPER",
+    //       2000,
+    //     ]}
+    //     speed={50}
+    //     className=" text-[#5F0F40]"
+    //     wrapper="span"
+    //     repeat={Infinity}
+    //   />
+    // </motion.div>{" "}
+    //     {/* <p className="text-white text text-lg">
+    //       I am a self-curious Web Developer as my educational background is
+    //       not related programming but enjoy Web Development so much, try to
+    //       develop the front end of a web application using React.js and the
+    //       back-end using Node.js(express) with comfortable. Besides always
+    //       ready to learn any new technology. Honestly, I am excited to start
+    //       my career as a web developer as well as to enhance my workplace and
+    //       reputation day by day.
+    //     </p> */}
+    // <button className="rounded my-8 bold py-3 px-7 border-t border-primaryLight2
+    //       bg-gradient-to-r from-[#5F0F40] to-black font-bold text-white">
+    //   <a href="Ratri khan2.pdf" download="myfile.pdf">
+    //     Download Resume
+    //   </a>
+    // </button>
+    //     <div>
+
+    //     </div>
+    //   </div>
+    // </div>
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: `url('${img}')`,
+      }}
+    >
+      {/* <div className="hero-overlay bg-opacity-60"></div> */}
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl text-[#00ffcc] ">Hi, I am Ratri Khan</h1>
           <motion.div
-            className="mb-6 text-[30px] lg:text-[45px] font-secondary font-bold uppercase leading-[1]mx-auto lg:mx-0"
+            className="mb-6 text-[30px] lg:text-[20px] font-secondary font-bold uppercase leading-[1]mx-auto lg:mx-0"
             variants={fadeIn("up", 0.3)}
             initial="hidden"
             whileInView={"show"}
@@ -26,32 +83,33 @@ const Banner = () => {
                 2000,
               ]}
               speed={50}
-              className=" text-[#5F0F40]"
+              className=" text-[#00ffcc]"
               wrapper="span"
               repeat={Infinity}
             />
           </motion.div>{" "}
-          <p className="text-white text text-lg">
-            I am a self-curious Web Developer as my educational background is
-            not related programming but enjoy Web Development so much, try to
-            develop the front end of a web application using React.js and the
-            back-end using Node.js(express) with comfortable. Besides always
-            ready to learn any new technology. Honestly, I am excited to start
-            my career as a web developer as well as to enhance my workplace and
-            reputation day by day.
-          </p>
-          <button className="rounded my-8 bold py-3 px-7 border-t border-primaryLight2
-                bg-gradient-to-r from-[#5F0F40] to-black font-bold text-white">
-            <a href="Ratri khan2.pdf" download="myfile.pdf">
-              Download Resume
-            </a>
+          <button>
+            <ul className="flex text-center">
+              <li className="w-12 h-12 border-2 m-1 rounded-full border-[#00ffcc]">
+                <FaLinkedinIn
+                  className="w-8 h-8 mt-1 ms-2 text-[#00ffcc]
+                      text-center"
+                />
+              </li>
+              <li className="w-12 h-12 border-2 m-1 rounded-full border-[#00ffcc]">
+                <FiGithub className="w-8 h-8 mt-2 ms-1 text-[#00ffcc]" />
+              </li>
+              <li className="w-12 h-12 border-2 m-1 rounded-full border-[#00ffcc]">
+                <FaFacebookF
+                  className=" w-8 h-8 mt-2 ms-2 text-[#00ffcc]
+                      text-center "
+                />
+              </li>
+            </ul>
           </button>
-          <div>
-              
-          </div>
         </div>
-          <img src={img} alt="" className="h-[520px] rounded "/>
       </div>
+    </div>
   );
 };
 
--- src/component/Home/Contact.jsx
+++ src/component/Home/Contact.jsx
@@ -1,14 +1,23 @@
-import { motion } from "framer-motion";
+import { motion } from "framer-motion"; 
 import { fadeIn } from "../../variants";
 import { BsTelephone } from "react-icons/bs";
 import { AiOutlineMail } from "react-icons/ai";
 import { CiLocationOn } from "react-icons/ci";
+import { FaFacebookF, FaLinkedinIn ,FaWhatsapp } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
+import "./banner.css";
+import AOS from 'aos';
 const Contact = () => {
   const contactobj = [
     {
       name: <BsTelephone />,
       title: "Phone",
-      des: "+8801374930262",
+      des: "01786641114",
+    },
+    {
+      name: <FaWhatsapp />,
+      title: "whats app",
+      des: "0178664114",
     },
     {
       name: <AiOutlineMail />,
@@ -22,42 +31,17 @@ const Contact = () => {
     },
   ];
   return (
-    <div className="container mx-auto my-7">
+    <div className="container w-10/12 mx-auto my-7 bg-black" style={{ height: '100vh' }}>
       <h2 className="text-white  text-center text-4xl font-bold font-sans  my-8">
         Contact with Me
       </h2>
-      <div className="flex lg:flex-row flex-col lg:justify-center lg:gap-x-40 items-center text-white ">
-        <motion.div
-          variants={fadeIn("right", 0.3)}
-          initial="hidden"
-          whileInView={"show"}
-          viewport={{ once: "false", amount: 0.7 }}>
-          <div>
-            {contactobj.map((index) => (
-              <div key={index.title}>
-                <div className="flex items-center gap-x-2">
-                  <div className="font-semibold text-xl">{index.name}</div>
-                  <div>
-                    <h2 className="text-2xl font-bold">{index.title}</h2>
-                    <p>{index.des}</p>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </motion.div>
+      {/* <div className="flex lg:flex-row flex-col lg:justify-center lg:gap-x-40 items-center text-white m-auto justify-between"> */}
+      <div className="flex justify-between">
+        
 
-        <motion.div
-          className="mt-5"
-          variants={fadeIn("left", 0.5)}
-          initial="hidden"
-          whileInView={"show"}
-          viewport={{ once: "false", amount: 0.7 }}
-        >
-          <form
-            className="flex-1 lg:w-[500px] p-10 rounded-xl shadow-lg border border-white shadow-white"
-            action="https://formspree.io/f/xpzgaoka"
-            method="POST">
+        <div className="w-6/12">
+          <form className="flex-1 lg:w-[500px] p-10 rounded-xl border" action="https://formspree.io/f/xpzgaoka"
+          method="POST">
             <input
               type="text"
               className="placeholder:text-white border-0 border-b bg-transparent w-full lg:max-w-[1000px] boxshadow-none outline-none focus:border-accent mb-8 pb-4 text-white"
@@ -85,17 +69,49 @@ const Contact = () => {
               id="Message"
               required>                
             </textarea>
-
-            {/* <input type="submit"placeholder='submit'/> */}
             <button className="rounded bold py-3 px-7 border-t border-primaryLight2
                 bg-gradient-to-r from-[#5F0F40] to-black font-bold text-white">
             send message
           </button>
           </form>
-        </motion.div>
+        </div>
+        <div className="contact w-6/12 rounded-xl text-white grid  place-items-center ">
+          <div><div>
+            {contactobj.map((index) => (
+              <div key={index.title} className="my-4">
+                <div className="items-center gap-x-2">
+                    <span className="font-semibold text-xl"></span>
+                    <div className="text-2xl font-bold flex items-center">{index.name}{index.title}</div>
+                    <p>{index.des}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <button>
+            <ul className="flex text-center">
+              <li className="w-10 h-10 border-2 m-1 rounded-full border-[#00ffcc]">
+                <FaLinkedinIn
+                  className="w-7 h-7 mt-1 ms-2 text-[#00ffcc]
+                      text-center"
+                />
+              </li>
+              <li className="w-10 h-10 border-2 m-1 rounded-full border-[#00ffcc]">
+                <FiGithub className="w-7 h-7 mt-2 ms-1 text-[#00ffcc]" />
+              </li>
+              <li className="w-10 h-10 border-2 m-1 rounded-full border-[#00ffcc]">
+                <FaFacebookF
+                  className=" w-7 h-7 mt-2 ms-1 text-[#00ffcc]
+                      text-center "
+                />
+              </li>
+            </ul>
+          </button></div>
+        </div>
       </div>
+      
     </div>
+    
   );
 };
-
+AOS.init();
 export default Contact;
